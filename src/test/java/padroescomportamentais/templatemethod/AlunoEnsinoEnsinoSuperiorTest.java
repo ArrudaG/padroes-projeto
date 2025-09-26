f2fb775d54e134ac26ca71e844a94be7c16beec6
Gabriel Arruda <gabrielarr1227@gmail.com>
2025-09-26 10:37:51 -0300

    Trabalho padrão de projeto bridge

--- src/test/java/padroescomportamentais/templatemethod/AlunoEnsinoEnsinoSuperiorTest.java
+++ src/test/java/padroescomportamentais/templatemethod/AlunoEnsinoEnsinoSuperiorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AlunoEnsinoSuperiorTest {
+class AlunoEnsinoEnsinoSuperiorTest {
 
     @Test
     void deveRetonarAprovado() {
